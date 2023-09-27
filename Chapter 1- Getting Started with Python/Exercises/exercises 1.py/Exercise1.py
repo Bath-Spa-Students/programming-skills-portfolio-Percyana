9279e6fae59093076f3bfef5788f5c414b368833
Percyana <percyguilhermekulyumba@gmail.com>
2023-09-27 14:13:45 +0400

    Exercise1.py
    Co-Authored-By: rafiagithub <142434667+rafiagithub@users.noreply.github.com>

--- Chapter 1- Getting Started with Python/Exercises/exercises 1.py/Exercise1.py
+++ Chapter 1- Getting Started with Python/Exercises/exercises 1.py/Exercise1.py
@@ -2,6 +2,6 @@
 
 print ("""Twinkle,twinkle,little star,
        how I Wonder what you are!
-       up above the world so high, like. a like a diamond in the sky . a diamond in the sky.
+       up above the world so high, like. a diamond in the sky.
     Twinkle,twinkle,little star, How I Wonder what you are""")
 
