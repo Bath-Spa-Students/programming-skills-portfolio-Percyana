02fdf124ffeb58d193bae2c6d2a56c7af55cf01f
Percyana <percyguilhermekulyumba@gmail.com>
2023-09-27 11:07:37 +0400

    exercise1.py
    Co-Authored-By: rafiagithub <142434667+rafiagithub@users.noreply.github.com>

---
+++ Chapter 1- Getting Started with Python/Exercises/exercises 1.py/Exercise1.py
@@ -0,0 +1,7 @@
+#Write a pythopn program to print the string in specific format
+
+print ("""Twinkle,twinkle,little star,
+       how I Wonder what you are!
+       up above the world so high, like. a like a diamond in the sky . a diamond in the sky.
+    Twinkle,twinkle,little star, How I Wonder what you are""")
+
